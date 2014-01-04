1722a152d1029b8b095016461c3984a55737b947
nilzen <olov@nilzen.se>
2014-01-04 14:50:30 +0100

    Dynamic controller loading WIP

--- public/js/app.js
+++ public/js/app.js
@@ -26,8 +26,41 @@ var app = angular.module('onmote', [
     ]);
   }
 ])
-.config(function ($routeProvider) {
+.config(function ($routeProvider, $locationProvider) {
+
   $routeProvider.when('/status', { templateUrl: 'plugin/status/index.html', controller: 'StatusCtrl' });
   $routeProvider.when('/telldus', { templateUrl: 'plugin/telldus/index.html', controller: 'TelldusDeviceListCtrl' });
+
+  $routeProvider.when('/:plugin', {
+
+    templateUrl: function(rd) {
+      return 'plugin/' + rd.plugin + '/index.html'
+    },
+    controller: function($route) { 
+
+      var plugin = $route.current.params.plugin
+
+      return plugin.charAt(0).toUpperCase() + plugin.slice(1) + 'Ctrl'
+    },
+    resolve: {
+      load: function($q, $route) {
+
+        var deferred = $q.defer();
+
+        var dependencies = [
+          'plugin/' + $route.current.params.plugin + '/controller.js'
+        ];
+
+        $script(dependencies, function () {
+          $rootScope.$apply(function() {
+            deferred.resolve();
+          });
+        });
+
+        return deferred.promise;
+      }
+    }
+  });
+
   $routeProvider.otherwise({redirectTo: '/telldus'});
 });
